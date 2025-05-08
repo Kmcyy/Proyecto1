0b4d0839b06b89c4b55235bfd69692504230612f
Kmcyy <150307204+Kmcyy@users.noreply.github.com>
2025-05-08 01:21:41 -0500

    Add files via upload

---
+++ Nosotros.tsx
@@ -0,0 +1,27 @@
+import "../css/Nosotros.css";
+
+function Nosotros() {
+  return (
+    <section id="Nosotros" className="padded">
+      <div className="container">
+        <h2>Nosotrmoilos</h2 >
+        
+        <h2>Inicio</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed cumque
+          labore autem laborum eius pariatur, voluptatum nesciunt obcaecati!
+          Nisi mollitia velit voluptatem impedit veritatis suscipit consectetur,
+          animi possimus amet aspernatur. Illo blanditiis corrupti, obcaecati
+          error cumque repellat rem in provident tempora alias qui vero
+          reprehenderit eius aperiam nostrum, iusto dolorum hic! Accusantium
+          veritatis maxime at eligendi eum vel sit nulla!
+        </p>
+        <img src="src/assets/images/pizzatexto.jpg" alt="" />
+      </div>
+    </section>
+  );
+}
+/*
+<img id="imagenfondo" src="src/assets/images/pizzafondo.jpg" alt="" />
+*/
+export default Nosotros;
